7338a2109ea6956356db941dd95ca3ba9084af8f
Basavanagowda Kanur <basavanagowda@gmail.com>
2021-10-19 22:23:17 +0530

    Change styles for circle

--- src/components/circle.tsx
+++ src/components/circle.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
 
 const clear = (node: any) => d3.select(node).selectAll('*').remove();
@@ -18,21 +18,51 @@ interface Props {
 }
 
 export default ({ radius }: Props) => {
-  const svgRef = useRef(null);
+  const [width, setWidth] = useState<number>(400);
+  const [height, setHeight] = useState<number>(400);
+  const [cx, setCx] = useState<number>(200);
+  const [cy, setCy] = useState<number>(200);
+  const svgRef = useRef<SVGSVGElement>(null);
 
   useEffect(() => {
     if (svgRef.current) {
       clear(svgRef.current);
-      drawCircle(svgRef.current, radius, 200, 200);
+      drawCircle(svgRef.current, radius, cx, cy);
     }
-  }, [svgRef, radius]);
+  }, [svgRef, radius, cx, cy]);
+
+  useEffect(() => {
+    if (svgRef.current) {
+      setWidth(svgRef.current!.clientWidth);
+    }
+  }, [svgRef]);
+
+  useEffect(() => {
+    const vh = Math.max(
+      document.documentElement.clientHeight || 0,
+      window.innerHeight || 0
+    );
+
+    setHeight(vh - (36 + 25 * 2 + 38 + 25 * 2));
+  });
+
+  useEffect(() => {
+    setCx(width / 2);
+  }, [width]);
+
+  useEffect(() => {
+    setCy(height / 2);
+  }, [height]);
 
   return (
     <svg
       ref={svgRef}
-      width='400px'
-      height='400px'
-      style={{ backgroundColor: 'yellow', border: '1px solid red' }}
+      width='100%'
+      height={`${height}px`}
+      style={{
+        backgroundColor: 'yellow',
+        border: '1px solid red',
+      }}
     />
   );
 };
--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -6,32 +6,35 @@ export default () => {
   const [radius, setRadius] = useState<number>(30);
 
   return (
-    <div className='row'>
-      <div className='col-8'>
-        <Circle radius={radius} />
-      </div>
-      <div className='col-4'>
-        <div className='row' />
-        <div className='btn-group' role='group' aria-label='Radius'>
-          <button
-            type='button'
-            className='btn btn-primary shadow-none'
-            onClick={() => setRadius(radius - 5)}
-          >
-            -
-          </button>
-          <button type='button' className='btn btn-primary' disabled>
-            {radius}
-          </button>
-          <button
-            type='button'
-            className='btn btn-primary shadow-none'
-            onClick={() => setRadius(radius + 5)}
-          >
-            +
-          </button>
+    <>
+      <div className='row'>
+        <div className='col-12'>
+          <div className='btn-group' role='group' aria-label='Radius'>
+            <button
+              type='button'
+              className='btn btn-primary shadow-none'
+              onClick={() => setRadius(radius - 5)}
+            >
+              -
+            </button>
+            <button type='button' className='btn btn-primary' disabled>
+              {radius}
+            </button>
+            <button
+              type='button'
+              className='btn btn-primary shadow-none'
+              onClick={() => setRadius(radius + 5)}
+            >
+              +
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+      <div className='row mt-4'>
+        <div className='col-12'>
+          <Circle radius={radius} />
+        </div>
+      </div>
+    </>
   );
 };
